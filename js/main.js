dc368fa0305eb01dd3db1e16d6e5fe5bc2f45d84
Genesis <>
2024-10-28 10:47:46 -0500

    Integrando Realtime Database to Form

--- js/main.js
+++ js/main.js
@@ -1,3 +1,36 @@
+const databaseURL = 'https://proyecto-82ab3-default-rtdb.firebaseio.com/collection.json';
+
+let sendData = () => {
+    // Obtén los datos del formulario
+    const formData = new FormData(form);
+    const data = Object.fromEntries(formData.entries()); // Convierte FormData a objeto
+
+    // new Date().toLocaleString( locales, options )
+    data['saved'] = new Date().toLocaleString('es-CO', { timeZone: 'America/Guayaquil' })
+
+    // Realiza la petición POST con fetch
+    fetch(databaseURL, {
+        method: 'POST', // Método de la solicitud
+        headers: {
+            'Content-Type': 'application/json' // Especifica que los datos están en formato JSON
+        },
+        body: JSON.stringify(data) // Convierte los datos a JSON
+    })
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(`Error en la solicitud: ${response.statusText}`);
+            }
+            return response.json(); // Procesa la respuesta como JSON
+        })
+        .then(result => {
+            alert('Agradeciendo tu preferencia, nos mantenemos actualizados y enfocados en atenderte como mereces'); // Maneja la respuesta con un mensaje
+            form.reset()
+        })
+        .catch(error => {
+            alert('Hemos experimentado un error. ¡Vuelve pronto!'); // Maneja el error con un mensaje
+        });
+}
+
 let ready = () => {
     console.log('DOM está listo')
 }
@@ -5,7 +38,7 @@ let ready = () => {
 let loaded = () => {
     //console.log('Iframes e Images cargadas')
     let myform = document.getElementById('form');
-    myform.addEventListener('submit', function(eventSubmit) {
+    myform.addEventListener('submit', function (eventSubmit) {
         eventSubmit.preventDefault();
         var emailElement = document.querySelector(".form-control-lg");
         var emailText = emailElement.value;
@@ -25,10 +58,13 @@ let loaded = () => {
                     //fill: 'forwards'  
                 }
             )
-          
+            return;
         }
     })
 }
 
 window.addEventListener("DOMContentLoaded", ready);
-window.addEventListener("load", loaded)
+window.addEventListener("load", loaded);
+
+
+
